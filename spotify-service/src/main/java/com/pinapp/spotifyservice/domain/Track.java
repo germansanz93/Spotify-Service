e18efe3ec0d9eab8e2befb78e8064cd08af18097
germansanz93 <germansanz93@gmail.com>
2021-11-03 20:30:50 -0300

    Album, Artist and Track entities Created

---
+++ spotify-service/src/main/java/com/pinapp/spotifyservice/domain/Track.java
@@ -0,0 +1,19 @@
+package com.pinapp.spotifyservice.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Track {
+  private Long id;
+  private String name;
+  private Long idArtist;
+  private Long idAlbum;
+  private Long reproduction;
+  private Long duration; //Duration in ms
+}
