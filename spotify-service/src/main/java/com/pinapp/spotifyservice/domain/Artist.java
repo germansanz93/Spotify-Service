8ffdbe937b0b0f89d4131aa7edd1313b4b082856
germansanz93 <germansanz93@gmail.com>
2021-11-04 11:06:41 -0300

    Artist service created and working

--- spotify-service/src/main/java/com/pinapp/spotifyservice/domain/Artist.java
+++ spotify-service/src/main/java/com/pinapp/spotifyservice/domain/Artist.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Artist {
-  private Long id;
+  private Long idArtist;
   private String name;
   private String genre;
   private String image;
